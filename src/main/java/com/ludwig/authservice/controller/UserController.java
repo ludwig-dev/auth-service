26558766762776e094f9eb373f62e43d17006bdd
Ludwig <00ludwighahn@gmail.com>
2025-03-10 13:08:46 +0100

    Add endpoint "/login" that generates a JWT token

--- src/main/java/com/ludwig/authservice/controller/UserController.java
+++ src/main/java/com/ludwig/authservice/controller/UserController.java
@@ -2,13 +2,17 @@ package com.ludwig.authservice.controller;
 
 import com.ludwig.authservice.model.User;
 import com.ludwig.authservice.service.UserService;
+import com.ludwig.authservice.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,6 +20,12 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user){
@@ -27,5 +37,19 @@ public class UserController {
 
         userService.registerNewUser(user);
         return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody User user){
+        Optional<User> foundUser = userService.findByUsername(user.getUsername());
+
+        if(foundUser.isEmpty())
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+
+        if(!passwordEncoder.matches(user.getPassword(), foundUser.get().getPassword()))
+            return new ResponseEntity<>("Invalid password", HttpStatus.UNAUTHORIZED);
+
+        String token = jwtUtil.generateToken(foundUser.get().getUsername());
+        return new ResponseEntity<>(token, HttpStatus.OK);
     }
 }
